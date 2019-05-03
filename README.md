bfe3f6f58d3b6fe8c6d5d27d459cbe76fce2e0b2
Christopher Sams <csams@redhat.com>
2019-05-03 16:04:10 -0500

    Initial commit with plugin definition, execution, and loading.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # rulz
-A simple IoC framework for system analysis
+A decorator based IoC framework for python3.6+. There are no runtime
+dependencies outside of what's in the standard library.
+
+A demo module is in `rulz/plugins/demo.py`.
+```python
+#!/usr/bin/env python
+from rulz import plugin, run_graph
+
+
+@plugin()
+def one():
+    return 1
+
+
+@plugin()
+def two():
+    return 2
+
+
+@plugin(one, two)
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    print(run_graph())
+```
+
+Invoke it directly or create a driver script like this:
+```python
+#!/usr/bin/env python
+from rulz import load_components, run_graph
+
+
+load_components("rulz.plugins")
+print(run_graph())
+```
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+runtime = set([
+])
+
+develop = set([
+    "flake8",
+    "ipython",
+    "pytest",
+    "setuptools",
+    "twine",
+    "wheel",
+])
+
+if __name__ == "__main__":
+    setup(
+        name="rulz",
+        version="0.0.1",
+        description="rulz is a IoC framework for system analysis",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/csams/rulz",
+        author="Christopher W. Sams",
+        packages=find_packages(),
+        install_requires=list(runtime),
+        package_data={"": ["LICENSE"]},
+        license="Apache 2.0",
+        extras_require={
+            "develop": list(runtime | develop),
+        },
+        classifiers=[
+            "Intended Audience :: Developers",
+            "Natural Language :: English",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7"
+        ],
+        include_package_data=True
+    )
